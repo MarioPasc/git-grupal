68be9ff058c83d4a472aac6cc69c57d6ebb01384
MarioPasc <mario.pg02@gmail.com>
2024-02-21 21:02:31 +0100

    Adicion metodo 1

--- Jarra.java
+++ Jarra.java
@@ -4,13 +4,18 @@ public class Jarra { //atributos
 //constructor
     public Jarra(int capacidadInicial){
         if(capacidadInicial<=0){
-            throw new RuntimeException("ERROR: no se pueden crear jarras
-            con capacidad menor o igual que cero.");
+            throw new RuntimeException("ERROR: no se pueden crear jarras con capacidad menor o igual que cero.");
         }
         this.capacidad=capacidadInicial;
     }
     // metodos
     // 1
+    public int capacidad(){
+        return this.capacidad;
+    }
+    public int cantidad(){
+        return this.cantidad;
+    }
     // 2
     // 3
     public void metodoComunitario(){

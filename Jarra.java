6dd65efad8a5a28f6c07d29bcd4866e7a30ecda9
GonzaloMA <gonzalo.mesas.aranda@uma.es>
2024-02-21 21:06:52 +0100

    Adicion metodo 3

--- Jarra.java
+++ Jarra.java
@@ -1,19 +1,31 @@
-public class Jarra { //atributos
+public class Jarra { // atributos
     private int cantidad;
     private int capacidad;
-//constructor
+
+    // constructor
     public Jarra(int capacidadInicial){
         if(capacidadInicial<=0){
-            throw new RuntimeException("ERROR: no se pueden crear jarras
-            con capacidad menor o igual que cero.");
+            throw new RuntimeException("ERROR: no se pueden crear jarras con capacidad menor o igual que cero.");
         }
         this.capacidad=capacidadInicial;
     }
+
     // metodos
     // 1
     // 2
     // 3
-    public void metodoComunitario(){
+    public void llenaDesde(Jarra j) {
+        while ((this.cantidad < this.capacidad) && (j.cantidad > 0)) {
+            this.cantidad++;
+            j.cantidad--;
+        }
+    }
+
+    public String toString() {
+        return ("(" + this.capacidad + "," + this.cantidad + ")");
+    }
+
+    public void metodoComunitario() {
         // Comentario incluido para pruebas
         System.out.println("Aqui escribimos todos");
     }
